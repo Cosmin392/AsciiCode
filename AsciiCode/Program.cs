3b9525ad2e4dd03c8b8bed23e7084c5ee63ceabe
Cosmin392 <cosminanghel7117@gmail.com>
2020-11-16 11:15:49 +0100

    AsciiCode

--- AsciiCode/Program.cs
+++ AsciiCode/Program.cs
@@ -6,7 +6,16 @@ namespace AsciiCode
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Inserisci un valore numerico o alfanumerico da convertire");
+            char carattere = Convert.ToChar(Console.ReadLine());
+
+            int number = carattere;
+
+            Console.WriteLine(number.ToString());
+
+            Console.ReadKey();
+
+
         }
     }
 }
